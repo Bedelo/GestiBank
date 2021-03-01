4e86c50e3bdbaf021d1c97ac2e50b4958f33c5fe
Bedelo <s_ceb@hotmail.com>
2021-03-01 21:08:24 +0100

    fix creation et connexion compte

--- app/src/main/java/com/sip/gestibank/CreationCompte.java
+++ app/src/main/java/com/sip/gestibank/CreationCompte.java
@@ -2,13 +2,45 @@ package com.sip.gestibank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class CreationCompte extends AppCompatActivity {
+
+    TextView toConnexion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation_compte);
+
+
+    }
+
+    public void callConnexionPage(View view){
+        Intent i = new Intent(getApplicationContext(), ConnexionCompte.class);
+        startActivity(i);
+    }
+
+    private void initToConnexion(){
+        toConnexion = (TextView) findViewById(R.id.textToConnexion);
+        SpannableString content = new SpannableString("Cliquez ici");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        String chaine = toConnexion.getText().toString();
+        toConnexion.setText(chaine.concat(content.toString()));
+
+        toConnexion.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v ) {
+                callConnexionPage(v);
+            }
+        });
     }
 }
