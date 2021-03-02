38af6ee88cb4528e7da0f803b779a208c62e1ba2
Bedelo <s_ceb@hotmail.com>
2021-03-02 09:30:37 +0100

    delete method using Spannable for message onto creation_compte

--- app/src/main/java/com/sip/gestibank/CreationCompte.java
+++ app/src/main/java/com/sip/gestibank/CreationCompte.java
@@ -19,8 +19,6 @@ public class CreationCompte extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation_compte);
-
-
     }
 
     public void callConnexionPage(View view){
@@ -28,19 +26,4 @@ public class CreationCompte extends AppCompatActivity {
         startActivity(i);
     }
 
-    private void initToConnexion(){
-        toConnexion = (TextView) findViewById(R.id.textToConnexion);
-        SpannableString content = new SpannableString("Cliquez ici");
-        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        String chaine = toConnexion.getText().toString();
-        toConnexion.setText(chaine.concat(content.toString()));
-
-        toConnexion.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v ) {
-                callConnexionPage(v);
-            }
-        });
-    }
 }
