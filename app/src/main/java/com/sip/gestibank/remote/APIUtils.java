42fbf89e8ecd6079ef30a1318816517a6967298b
Bedelo <s_ceb@hotmail.com>
2021-03-02 11:41:12 +0100

    add retrofit to convertisseur

---
+++ app/src/main/java/com/sip/gestibank/remote/APIUtils.java
@@ -0,0 +1,13 @@
+package com.sip.gestibank.remote;
+
+public class APIUtils {
+
+    private APIUtils(){
+    };
+
+    public static final String API_URL = "http://192.168.43.165:86/api/";
+
+    public static UserService getUserService(){
+        return RetrofitClient.getClient(API_URL).create(UserService.class);
+    }
+}
